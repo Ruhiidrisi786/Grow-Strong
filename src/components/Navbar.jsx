350128f0a5a9ddee4618c6575a941a1386a966e7
Ruhiidrisi786 <rukaiyabano888@gmail.com>
2026-06-15 07:43:49 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,53 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <div className="logo">GROW STRONG</div>
+      <div className="logo">
+        <Link to="/">GROW STRONG</Link>
+      </div>
 
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
 
-      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink to="/" onClick={closeMenu}>
+            Home
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/about">About</Link>
+          <NavLink to="/about" onClick={closeMenu}>
+            About
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/classes">Classes</Link>
+          <NavLink to="/classes" onClick={closeMenu}>
+            Classes
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/trainers">Trainers</Link>
+          <NavLink to="/trainers" onClick={closeMenu}>
+            Trainers
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/contact">Contact</Link>
+          <NavLink to="/contact" onClick={closeMenu}>
+            Contact
+          </NavLink>
         </li>
       </ul>
     </nav>
